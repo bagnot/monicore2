24b7a31b439907a87e81b7e01dea15c2aa47aa10
bagnot <charismaebagnot21@gmail.com>
2026-05-21 00:50:47 +0800

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:candiarugie@localhost:5432/monicore_db")
 
-# Render gives a postgres:// URL, SQLAlchemy needs postgresql://
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
